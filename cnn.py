e1b3cf054965b20f1a1aa66e8851f9e556152c0b
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2019-05-19 21:58:56 +0100

    Class Convolutional Neural Network working

--- cnn.py
+++ cnn.py
@@ -4,6 +4,8 @@ import numpy as np
 
 class CNN:
     """
+        name:
+            - an unique name used to create a variable scope
         imageWidth:
             - the width of the images we are going to train
         imageHeight:
@@ -18,68 +20,125 @@ class CNN:
         poolSize:
             - max pooling size
             - example: (2,2) performs max pooling over a 2x2 matrix
+        learningRate:
+            - the learning rate
+        decay:
+            - RMSProp decay. How much to decay the current momentum
+        momentum:
+            - the momentum
     """
-    def __init__(self, imageWidth, imageHeight, hiddenSize, outputSize, filters=None, poolSize=(2, 2)):
+    def __init__(self, name, imageWidth, imageHeight, hiddenSize, outputSize, filters=None, poolSize=(2, 2), learningRate = 0.0001, decay=0.99, momentum=0.9):
+        self.name = name
+        self.momentum = momentum
+        self.decay = decay
+        self.learningRate = learningRate
         self.annHiddenSize = hiddenSize
         self.imageHeight = imageHeight
         self.imageWidth = imageWidth
         self.annOutputSize = outputSize
         self.poolSize = [1, poolSize[0], poolSize[1], 1]
         self.filters = filters
+        self.session = tf.Session()
+
+        with tf.name_scope(self.name):
+            # input and target placeholders
+            self.X = tf.placeholder(tf.float32, shape=(None, imageWidth, imageHeight, 3), name='X')
+            self.T = tf.placeholder(tf.int32, shape=(None, self.annOutputSize), name='T')
+
+            # layers
+            self.convLayers = []
+
+            for filter in self.filters:
+                if len(self.convLayers) == 0:
+                    layer = ConvolutionalLayer(self.X, filter, self.poolSize)
+                else:
+                    layer = ConvolutionalLayer(self.convLayers[-1].Z, filter, self.poolSize)
+                self.convLayers.append(layer)
+
+            # calculate the ann number of input neurons based on the previous pooling layers
+            Z_last = self.convLayers[-1].Z
+            Z_last_shape = Z_last.get_shape().as_list()
+            self.annInputSize = np.prod(Z_last_shape[1:])
+
+            # ann layers (we use just one hidden layer for simplicity, we can tweak the number of neurons in it)
+            # in total we have 3 ann layers, the input layer, the hidden layer and the output layer
+            # vanilla ANN weights, the weights are initialized according to the Xavier/Glorot's initialization method
+            # original paper: http://proceedings.mlr.press/v9/glorot10a/glorot10a.pdf
+            Wh_init = np.random.randn(self.annInputSize, self.annHiddenSize) / np.sqrt(self.annInputSize + self.annHiddenSize)
+            bh_init = np.zeros(self.annHiddenSize, dtype=np.float32)
+            Wo_init = np.random.randn(self.annHiddenSize, self.annOutputSize) / np.sqrt(self.annHiddenSize + self.annOutputSize)
+            bo_init = np.zeros(self.annOutputSize, dtype=np.float32)
+
+            Wh = tf.Variable(Wh_init.astype(np.float32), name="hidden_weights")
+            bh = tf.Variable(bh_init.astype(np.float32), name="hidden_bias")
+            Wo = tf.Variable(Wo_init.astype(np.float32), name="output_weights")
+            bo = tf.Variable(bo_init.astype(np.float32), name="output_bias")
+
+            Z_last = self.convLayers[-1].Z
+            Z_last_shape = Z_last.get_shape().as_list()
+
+            Zr = tf.reshape(Z_last, [-1, np.prod(Z_last_shape[1:])])
+            Zh = tf.nn.relu(tf.matmul(Zr, Wh) + bh)
+            self.Yish = tf.matmul(Zh, Wo) + bo
 
 
-        # input and target placeholders
-        self.X = tf.placeholder(tf.float32, shape=(None, imageWidth, imageHeight, 3), name='X')
-        self.T = tf.placeholder(tf.int32, shape=(None, self.annOutputSize), name='T')
-
-        # layers
-        self.convLayers = []
-
-        for filter in self.filters:
-            if len(self.convLayers) == 0:
-                layer = ConvolutionalLayer(self.X, filter, self.poolSize)
-            else:
-                layer = ConvolutionalLayer(self.convLayers[-1].Z, filter, self.poolSize)
-            self.convLayers.append(layer)
-
-        # calculate the ann number of input neurons based on the previous pooling layers
-        Z_last = self.convLayers[-1].Z
-        Z_last_shape = Z_last.get_shape().as_list()
-        self.annInputSize = np.prod(Z_last_shape[1:])
-
-        # ann layers (we use just one hidden layer for simplicity, we can tweak the number of neurons in it)
-        # in total we have 3 ann layers, the input layer, the hidden layer and the output layer
-        # vanilla ANN weights, the weights are initialized according to the Xavier/Glorot's initialization method
-        # original paper: http://proceedings.mlr.press/v9/glorot10a/glorot10a.pdf
-        Wh_init = np.random.randn(self.annInputSize, self.annHiddenSize) / np.sqrt(self.annInputSize + self.annHiddenSize)
-        bh_init = np.zeros(self.annHiddenSize, dtype=np.float32)
-        Wo_init = np.random.randn(self.annHiddenSize, self.annOutputSize) / np.sqrt(self.annHiddenSize + self.annOutputSize)
-        bo_init = np.zeros(self.annOutputSize, dtype=np.float32)
-
-        Wh = tf.Variable(Wh_init.astype(np.float32))
-        bh = tf.Variable(bh_init.astype(np.float32))
-        Wo = tf.Variable(Wo_init.astype(np.float32))
-        bo = tf.Variable(bo_init.astype(np.float32))
-
-        Z_last = self.convLayers[-1].Z
-        Z_last_shape = Z_last.get_shape().as_list()
-
-        Zr = tf.reshape(Z_last, [-1, np.prod(Z_last_shape[1:])])
-        Zh = tf.nn.relu(tf.matmul(Zr, Wh) + bh)
-        self.Yish = tf.matmul(Zh, Wo) + bo
-
-
-        # cost function
-        cost = tf.reduce_sum(
-            tf.nn.softmax_cross_entropy_with_logits_v2(
-                logits=self.Yish,
-                labels=self.T
+            # cost function
+            self.cost = tf.reduce_sum(
+                tf.nn.softmax_cross_entropy_with_logits_v2(
+                    logits=self.Yish,
+                    labels=self.T
+                )
             )
-        )
 
-        # train function
-        self.trainOp = tf.train.RMSPropOptimizer(0.0001, decay=0.99, momentum=0.9).minimize(cost)
+            # train operation
+            self.trainOp = tf.train.RMSPropOptimizer(learning_rate=self.learningRate, decay=self.decay, momentum=self.momentum).minimize(self.cost)
 
+            # predict operation
+            self.predictOp = tf.argmax(self.Yish, 1)
+
+        # initialize variables
+        init = tf.global_variables_initializer()
+
+        self.session.run(init)
+
+    def train(self, X_train, y_train, X_test, y_test, batchSize = 128, epochs = 6):
+        N = len(X_train)
+        n_batches = N//batchSize
+
+        error = []
+        accuracy = []
+        for e in range(0, epochs):
+            epochErr = 0
+            for b in range(0, n_batches):
+                print(
+                    "\rEpoch " + str(e + 1) + " of " + str(epochs) + " | Training batch " + str(b + 1) + " of " + str(
+                        n_batches), end="")
+
+                Xbatch = X_train[b * batchSize:(b * batchSize + batchSize), ]
+                Ybatch = y_train[b * batchSize:(b * batchSize + batchSize), ]
+
+                self.session.run(self.trainOp, feed_dict={self.X: Xbatch, self.T: Ybatch})
+                epochErr += self.session.run(self.cost, feed_dict={self.X: Xbatch, self.T: Ybatch})
+
+            # after one epoch calculates the accuracy
+            testSucc = 0
+            testAcc = 0
+            for k in range(0, len(y_test) // batchSize):
+                Xbatch = X_test[k * batchSize:(k + 1) * batchSize, ]
+                Ybatch = y_test[k * batchSize:(k + 1) * batchSize, ]
+
+                prediction = self.session.run(self.predictOp, feed_dict={self.X: Xbatch, self.T: Ybatch})
+                testSucc += np.sum(prediction == np.argmax(Ybatch))
+                testAcc = testSucc / len(y_test)
+
+            error.append(epochErr)
+            accuracy.append(testAcc)
+            print(" | Epoch error: {0:.0f}".format(epochErr) + " | Accuracy: {0:.2f}".format(testAcc))
+
+        return (error, accuracy)
+
+    def close(self):
+        self.session.close()
 
 class ConvolutionalLayer:
     def __init__(self, X, filter, poolSize):
--- fruit_classification_tf_v2.py
+++ fruit_classification_tf_v2.py
@@ -11,16 +11,38 @@ from sklearn.preprocessing import OneHotEncoder
 from cnn import CNN
 from dataset_util import load_data
 
+# Normalizes images 0 - 255 -> 0 - 1
+def normalize(X):
+    # X of shape (N, W, H, 3)
+    return (X / 255).astype(np.float32)
 
 def main():
+    # loads, encodes and normalizes the dataset
+    X_train, y_train, X_test, y_test, N_class = load_data()
+    encoder = OneHotEncoder(sparse=True)
+    y_train = encoder.fit_transform(y_train.reshape(-1, 1)).toarray()
+    y_test = encoder.transform(y_test.reshape(-1, 1)).toarray()
+    X_train = normalize(X_train)
+    X_train, y_train = shuffle(X_train, y_train)
+    X_test = normalize(X_test)
+
+
+
     nn = CNN(
-            imageWidth=50,
-            imageHeight=50,
+            name="test",
+            imageWidth=100,
+            imageHeight=100,
             hiddenSize=256,
             outputSize=4,
             filters=[(50, 50, 3, 20), (25, 25, 20, 50), (10, 10, 50, 100)],
-            poolSize=(2,2)
+            poolSize=(2,2),
+            learningRate=0.0001,
+            decay=0.99,
+            momentum=0.90
         )
+
+    nn.train(X_train, y_train, X_test, y_test, batchSize=128, epochs=6)
+    nn.close()
 
 
 
